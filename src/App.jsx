d6d4654677da1b36172937fe5e20bb37a8f6ce75
Chauhan Uvesh <chauhanuvesh1221@gmail.com>
2026-04-30 05:36:38 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,9 @@ const animeData = [
   { title: "Bleach", image: "./Bleach.jpg" },
 
   { title: "Class Room Of The Elite", image: "./Ayano.jpg" },
+
+  { title: "Solo Leveling", image: "./Solo.jpg" },
+  { title: "Death Note", image: "./death.jpg" },
 ];
 
 export default function App() {
