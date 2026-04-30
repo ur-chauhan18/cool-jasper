128103d0e4dd54f8c7e5bf509adba186a0813ef9
Chauhan Uvesh <chauhanuvesh1221@gmail.com>
2026-04-30 06:05:30 +0000

    fix

--- src/src/App.js
+++ src/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../Home";
-import AnimeDetails from "../AnimeDetails";
+import Home from "./Home";
+import AnimeDetails from "./AnimeDetails";
 
 export default function App() {
   return (
